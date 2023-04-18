f1cb9a75223a17e8169f65bf32889a5e09c9f1e8
Pierre Noilhan <pierre@flowfactory.fr>
2023-04-18 10:53:42 +0200

    initialize roullier styles

---
+++ Roullier/HAMagnesie.js
@@ -0,0 +1,44 @@
+/*
+ * Custom style profile 
+ * Powered By Flow Factory
+ *
+ */
+
+
+const HEADER_LOGO_VIEW_NAME = 'SW_header';
+
+$(document).ready(function() {
+    console.log('ready fired - v3.0');
+    enableTheme();
+    render();
+});
+
+enableTheme = () => {
+    console.log('LOG - enable theme log');
+    $('body').addClass('ff');
+    $('form').addClass('ff');
+    $('.runtime-content').addClass('ff');
+    $('.runtime-form').addClass('ff');
+}
+
+render = () => {
+    renderHeader();
+    renderMenuItems();
+}
+
+renderHeader = () => {    
+	$('div[name="' + HEADER_LOGO_VIEW_NAME + '"]').closest('.row').addClass('header');
+	$('.header').insertBefore($('.runtime-content'));
+}
+
+renderMenuItems = () => {
+    $('a[name^="btn_menu"]').on('click', function (e) {
+        console.log(this.name); 
+        $('a[name^="btn_menu"]').removeClass('ff-menuactive');
+        $('a[name="' + this.name + '"]').addClass('ff-menuactive');
+        
+    });
+
+    $('a[name^="btn_menu"]').addClass('ff-menubutton');
+    $('a[name="btn_menu_Home"]').addClass('ff-menuactive');
+}
